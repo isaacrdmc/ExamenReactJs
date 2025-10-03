854ef88b7f52fcfee4a8293ba71624cfbe12f1f3
Isaac Ramirez <isaacrdmc@gmail.com>
2025-10-03 17:52:46 -0600

    Creando la card para la web

---
+++ src/card.jsx
@@ -0,0 +1,30 @@
+
+
+import { Card, Image } from 'react-bootstrap';
+
+
+export default function PokeCard(pokemon) {
+    return (
+        <div className="card mi-card m-2">
+            {/* Seccion de la imágen: */}
+            <Image
+                src={pokemon.sprites?.front_default}
+                className="card-img-top"
+                alt={pokemon.name}
+            />
+
+
+            {/* Sección del contenido: */}
+            <div className="card-Body">
+                <h4 className="card-title">{pokemon.nombre}</h4>
+                <br />
+                <h4 className="card-title">{pokemon.peso}</h4>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
