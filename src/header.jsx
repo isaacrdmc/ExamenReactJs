360defce7fa27a369dc773ae1cedbeacafc00cbb
Isaac Ramirez <isaacrdmc@gmail.com>
2025-10-03 17:38:13 -0600

    Ya tengo el primer componente con react y bootstrap

---
+++ src/header.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+// ^ Vamos a crear componente para el Headder:
+export default function Header(titulo) {
+    return (
+
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                {/* <image src="https://art.pixilart.com/023590844cc0a8b.png"/> */}
+                <a className="navbar-brand" href="#">
+                    {titulo.titulo}
+                </a>
+            </div>
+        </nav>
+    )
+}
